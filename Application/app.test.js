602b04a9ff9870af357d23a9e50fff3f6c1e18b3
Joel Ogualiri <joe00852@tjx.com>
2021-11-15 13:38:59 -0500

    added a test for homepage

--- Application/app.test.js
+++ Application/app.test.js
@@ -2,34 +2,34 @@ const request = require('supertest');
 const app = require('./app');
 
 
-describe('CUSTOMERS', ()=>{
+describe('CUSTOMERS', () => {
 
-    describe('GET method for CUSTOMER endpoint', ()=>{
+    describe('GET method for CUSTOMER endpoint', () => {
 
         //------------TEST FOR ALL CUSTOMERS---------------------------------+
-    
-        test("respond with a 200 status code", async () =>{
+
+        test("respond with a 200 status code", async () => {
             const response = await request(app).get("/api/customers")
             expect(response.statusCode).toBe(200)
         })
-    
-       test("the response must be an array of customers", async ()=>{
-           const response = await request(app).get("/api/customers")
-           expect(Array.isArray(response.body.customers)==true).toBe(true)
-       })
-       test("the response must have at least one customer", async ()=>{
-        const response = await request(app).get("/api/customers")
-        expect(response.body.customers).not.toHaveLength(0)
+
+        test("the response must be an array of customers", async () => {
+            const response = await request(app).get("/api/customers")
+            expect(Array.isArray(response.body.customers) == true).toBe(true)
+        })
+        test("the response must have at least one customer", async () => {
+            const response = await request(app).get("/api/customers")
+            expect(response.body.customers).not.toHaveLength(0)
         })
 
-    //for the homepage
+        //for the homepagr - check that the front page is uploaded
+        test("check that it returns the homepage", async () => {
+            const response = await request(app).get("/")
+            expect(response.header["content-type"]).toBe('text/html; charset=utf-8')
+        })
 
 
 
-})
-})
 
-describe('ORDERS', ()=>{
-//Dhrivit
-
+    })
 })
